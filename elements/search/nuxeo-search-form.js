2ff79541d45e748214d243f500160244d4ea8fc2
Gabriel Barata <gbarata@nuxeo.com>
2020-02-27 18:30:06 +0000

    NXP-24967: allow saved searches to be loaded via uid url

--- elements/search/nuxeo-search-form.js
+++ elements/search/nuxeo-search-form.js
@@ -568,6 +568,11 @@ Polymer({
      * If `true`, aggregagtes from page provider definition will not be computed.
      */
     skipAggregates: Boolean,
+
+    searches: {
+      type: Array,
+      value: [],
+    },
   },
 
   observers: [
@@ -728,6 +733,7 @@ Polymer({
       // if the view is not initialized yet, navigating to the search will trigger a search and display the results
       this.navigateTo('search', this.searchName);
     }
+    return Promise.resolve();
   },
 
   _reset() {
@@ -904,5 +910,17 @@ Polymer({
 
   _displayQuickFilters() {
     return this._quickFilters && this._quickFilters.length > 0;
+  },
+
+  _loadSavedSearch(id) {
+    if (id) {
+      (this.searches.length === 0 ? this.$['saved-searches'].get() : Promise.resolve()).then(() => {
+        this.selectedSearchIdx = this.searches.findIndex((s) => s.id === id) + 1;
+        if (this.results) {
+          // XXX rely on debouncer to update the results request with the saved search params
+          this._fetch(this.results);
+        }
+      });
+    }
   },
 });
